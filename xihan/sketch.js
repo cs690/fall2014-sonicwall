ad520381916451704fb5e508327cf6a54c7cc504
Azure22 <hanx.azure22@gmail.com>
2014-09-23 14:04:25 -0700

    class Label v1

--- xihan/sketch.js
+++ xihan/sketch.js
@@ -1,17 +1,17 @@
-var center = [100, 500];
+var origin = [100, 500];
 
-var axisV = new Axis(center, 450, -5);
+var axisV = new Axis(origin, 450);
 axisV.rotation = -Math.PI/2;
 axisV.setMark(25);
+axisV.markWidth = -5;
 //axisV.showAxis = false;
 axisV.showFirstMark = true;
 
-var axisH = new Axis(center, 600, 5);
+var axisH = new Axis(origin, 600);
 axisH.setMark(6);
 axisH.showMark = false;
 
-//
-var bar1 = new Bar(local2GlobalPoint(center, axisH.rotation, axisH.markSlot[1]), 20, 200);
+var bar1 = new Bar(local2GlobalPoint(origin, axisH.rotation, axisH.markSlot[1]), 20, 200);
 bar1.rotation = axisV.rotation;
 bar1.color = [255,215,0];
 
@@ -23,10 +23,16 @@ var bar3 = new Bar(local2GlobalPoint(bar2.startPoint, bar2.rotation, bar2.topPoi
 bar3.rotation = axisV.rotation;
 bar3.color = [205,127,50];
 
-var tag = new Tag([100, 500], 60, 80);
-tag.rotation = -Math.PI/2;
-tag.startPoint = local2GlobalPoint(bar3.startPoint, bar3.rotation, bar3.topPoint);
+var tag1 = new Tag(local2GlobalPoint(bar1.startPoint, bar1.rotation, bar1.rightPoint), 60, 80);
 
+var label1 = new Label(local2GlobalPoint(tag1.startPoint, tag1.rotation, [12 + tag1.height/2, 5]), "Gold: ??");
+label1.align = constants.CENTER;
+
+var tag2 = new Tag(local2GlobalPoint(bar3.startPoint, bar3.rotation, bar3.topPoint), 80, 40);
+tag2.rotation = bar3.rotation;
+
+var label2 = new Label(local2GlobalPoint(tag2.startPoint, tag2.rotation, [26, 0]), "Total: ??");
+label2.align = constants.CENTER;
 
 function setup()
 {
@@ -55,41 +61,40 @@ function draw()
 	bar2.draw();
 	bar3.draw();
 	
+	tag1.draw();
+	tag2.draw();
 	
-	tag.draw();
+	label1.draw();
+	label2.draw();
 }
 
-//Center means global center
-//Local center always starts from [0, 0]
-function local2GlobalPoint(center, rotation, localPoint)
+//Local coordinate always starts from [0, 0]
+function local2GlobalPoint(globalStartPoint, rotation, localPoint)
 {
 	result = [];
-	result[0] = center[0] + localPoint[0] * Math.cos(rotation) - localPoint[1] * Math.sin(rotation);
-    result[1] = center[1] + localPoint[0] * Math.sin(rotation) + localPoint[1] * Math.cos(rotation);
+	result[0] = globalStartPoint[0] + localPoint[0] * Math.cos(rotation) - localPoint[1] * Math.sin(rotation);
+    result[1] = globalStartPoint[1] + localPoint[0] * Math.sin(rotation) + localPoint[1] * Math.cos(rotation);
 	return result;
 }
 
 //Class Axis
-function Axis(startPoint, length, markLength)
+function Axis(startPoint, length)
 {
 	//Attributes
 	this.startPoint = [0, 0];
 	this.length = 0;
 	this.rotation = 0;
-	this.markSide = "left";//"left", "right", "both"
 	this.markSlot = [];
-	this.markLength = 0;
+	this.markWidth = 5;
 	this.showAxis = true;
 	this.showMark = true;
 	this.showFirstMark = false;
 	
 	//Construction
-	this.construct = function (startPoint, length, markLength)
+	this.construct = function (startPoint, length)
 	{
 		this.startPoint = startPoint;
 		this.length = length;
-		this.startPoint = startPoint;
-		this.markLength = markLength;
 	};
 	
 	//Methods
@@ -138,7 +143,7 @@ function Axis(startPoint, length, markLength)
 					{
 						push();
 						translate(this.markSlot[i][0], this.markSlot[i][1]);
-						line(0, 0, 0, this.markLength);
+						line(0, 0, 0, this.markWidth);
 						pop();
 					}
 				}
@@ -149,7 +154,7 @@ function Axis(startPoint, length, markLength)
 	}
 	
 	//Call construct
-	this.construct(startPoint, length, markLength);
+	this.construct(startPoint, length);
 }
 
 //Class Bar
@@ -158,6 +163,8 @@ function Bar(startPoint, width, height)
 	//Attributes
 	this.startPoint = [0, 0];
 	this.topPoint = [0, 0];
+	this.leftPoint = [0, 0];
+	this.rightPoint = [0, 0];
 	this.width = 0;
 	this.height = 0;
 	this.rotation = 0;
@@ -170,22 +177,29 @@ function Bar(startPoint, width, height)
 		this.startPoint = startPoint;
 		this.width = width;
 		this.height = height;
-		this._updateTopPoint();
+		this._updatePoints();
 	};
 	
 	//Methods
 	if (typeof this._initialized == "undefined")
 	{
+		Bar.prototype.setWidth = function (width)
+		{
+			this.width = width;
+			this._updatePoints();
+		}
 		
 		Bar.prototype.setHeight = function (height)
 		{
 			this.height = height;
-			this._updateTopPoint();
+			this._updatePoints();
 		}
 		
-		Bar.prototype._updateTopPoint = function ()
+		Bar.prototype._updatePoints = function ()
 		{
 			this.topPoint = [this.height, 0];
+			this.leftPoint = [this.height/2, -this.width/2];
+			this.rightPoint = [this.height/2, this.width/2];
 		}
 		
 		/*
@@ -242,16 +256,19 @@ function Tag(startPoint, width, height)
 	
 		Tag.prototype.draw = function ()
 		{
-			push();
-			translate(this.startPoint[0], this.startPoint[1]);
-			rotate(this.rotation);
-			noStroke();
-			fill(this.color[0], this.color[1], this.color[2]);
-			//[2, 0]
-			triangle(6, 0, 12, 5, 12, -5);
-			//[6, 0]
-			rect(12, -this.width/2, this.height, this.width);
-			pop();
+			if(this.show)
+			{
+				push();
+				translate(this.startPoint[0], this.startPoint[1]);
+				rotate(this.rotation);
+				noStroke();
+				fill(this.color[0], this.color[1], this.color[2]);
+				//[2, 0]
+				triangle(6, 0, 12, 5, 12, -5);
+				//[6, 0]
+				rect(12, -this.width/2, this.height, this.width);
+				pop();
+			}
 		};
 		
 		this._initialized = true;
@@ -260,3 +277,49 @@ function Tag(startPoint, width, height)
 	//Call construct
 	this.construct(startPoint, width, height);
 }
+
+function Label(startPoint, info)
+{
+	//Attributes
+	this.startPoint = [0, 0];
+	this.rotation = 0;
+	this.info = "";
+	this.size = 15;
+	this.align = constants.LEFT;
+	this.color = [0, 0, 0];
+	this.show = true;
+	
+	//Constructor
+	this.construct = function (startPoint, info)
+	{
+		this.startPoint = startPoint;
+		this.info = info;
+	};
+	
+	//Methods
+	if (typeof this._initialized == "undefined")
+	{
+	
+		Label.prototype.draw = function ()
+		{
+			if(this.show)
+			{
+				push();
+				translate(this.startPoint[0], this.startPoint[1]);
+				rotate(this.rotation);
+				noStroke();
+				textSize(this.size);
+				fill(this.color[0], this.color[1], this.color[2]);
+				textAlign(this.align);
+				text(this.info, 0, 0);
+				pop();
+			}
+		};
+		
+		this._initialized = true;
+	}
+	
+	//Call construct
+	this.construct(startPoint, info);	
+}
+
