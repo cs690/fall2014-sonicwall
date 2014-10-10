a9ffdb5a7bc1f03ef26f652dfaa9614300356502
Mich Gao <michellegao715@gmail.com>
2014-10-10 16:09:41 -0700

    Error Invalid value for <path> attribute

--- Michelle/WireSharkDataVisualization/default.js
+++ Michelle/WireSharkDataVisualization/default.js
@@ -1,5 +1,4 @@
-var maxheight = 15000;
-// HTTP protocol
+/* HTTP protocol */
 $(function() {
   var target_protocol = "HTTP";
   var margin = {top: 20, right: 20, bottom: 30, left: 70},
@@ -8,33 +7,23 @@ $(function() {
 
   var x = d3.scale.linear()
       .range([0, width]);
-
   var y = d3.scale.linear()
       .range([height, 0]);
-
   var xAxis = d3.svg.axis()
       .scale(x)
       .orient("bottom");
-
   var yAxis = d3.svg.axis()
       .scale(y)
       .orient("left");
-
   var color = d3.scale.category20();
-
-  /*var HTTP_tip = d3.tip()
-	  .attr('class','d3-tip')
-	  .offset([0,10])
-	  .html(function(d) {
-		  return ""+
-		  "<strong>Year:</strong><span style='color:red'>"+d.ekei+"</span>"+"<br/>"
-		  //"<div>HTTP: "+d.HTTP+"</div>"
-	}); */
-
+  
   var area = d3.svg.area()
-      .x(function(d) { return x(d.x); })
-      .y0(height)
-      .y1(function(d) { return y(d.y); });
+      .x(function(d) { return x(d.TIME_SPAN); })
+      .y0(function(d) {return y(d.y0); })
+      .y1(function(d) { return y(d.y0 + d.y); });
+
+  var stack = d3.layout.stack()
+	  .values(function(d) {return d.values; });
 
   var svg = d3.select("body").append("svg")
       .attr("width", width + margin.left + margin.right)
@@ -43,49 +32,38 @@ $(function() {
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   d3.tsv("MultipleProtocol.tsv", function(error, data) {
-    data = data.map(function(d) {
-      d.x = parseInt(d.TIME_SPAN);
-      d.y = parseInt(d.HTTP);
-      return d;
-    });
-
-	//svg.call(HTTP_tip);
-
-    x.domain(d3.extent(data, function(d) { return d.x; }));
-    y.domain([0, d3.max(data.map(function(d) { return d.y; })) * 1.1]);
-	//y.domain([0,3500000]);
-
-    svg.append("path")
-        .datum(data)
-        .attr("class", "area")
-        .attr("d", area)
-	  	.style("fill", '#2ECCFA');
-        //.style("fill", function(d) { return color(18); });
-	  	//.on('mouseover', HTTP_tip.show)
-	  	//.on('mouseout', HTTP_tip.hide);
-
-    var legend = svg.append("g")
-        .attr("class", "legend")
-        .attr("x", width-65)
-        .attr("y", 25)
-        .attr("height", 300)
-        .attr("width", 300);
-	
-
-    legend.append("rect")
-      .attr("x", width - 65)
-      .attr("y", 25)
-      .attr("width", 14)
-      .attr("height", 14)
-      .style("fill", '#2ECCFA');
+	  color.domain(d3.keys(data[0]).filter(function(k) {return k != "TIME_SPAN"; }));
 	  
-  	  legend.append("text")
-      .attr("x", width-65)
-      .attr("y", 25)
-	  .style("font-size","16px")
-      .text("HTTP");
+  	  //TODO what does name and d[name] means???   and browsers???
+	  var browsers = stack(color.domain().map(function(name) {
+		  return {
+			  name : name,
+		      values : data.map(function(d) {
+				  return {date: d.TIME_SPAN, y:d[name]/100};
+			  })
+		  };
+	  }));
 
-    svg.append("g")
+    x.domain(d3.extent(data, function(d) { return d.TIME_SPAN; }));
+    //TODO  what does this browser mean?
+	var browser = svg.selectAll(".browser")
+			.data(browsers)
+			.enter().append("g")
+			.attr("class","browser");
+  
+	browser.append("path")
+			.attr("class", "area")
+			.attr("d", function(d) {return area(d.values); })
+			.style("fill", function(d) {return color(d.name); });
+
+	browser.append("text")
+		.datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
+		.attr("transform", function(d) { return "translate(" + x(d.value.TIME_SPAN) + "," + y(d.value.y0 + d.value.y / 2) + ")"; })
+        .attr("x", -6)
+		.attr("dy", ".35em")
+        .text(function(d) { return d.name; });
+	
+	svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
@@ -93,8 +71,7 @@ $(function() {
     svg.append("g")
         .attr("class", "y axis")
         .call(yAxis);
-  });
-
+	});
 });
 
 // DNS protocol 
