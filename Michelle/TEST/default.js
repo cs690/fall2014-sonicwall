a13e5531369d79603070d825d0a22188c04bd82e
Mich Gao <michellegao715@gmail.com>
2014-10-10 10:57:40 -0700

    add default.js

--- Michelle/TEST/default.js
+++ Michelle/TEST/default.js
@@ -1,10 +1,17 @@
 $(function() {
 	d3.tsv("DNS.tsv", function(data) {
+	/*Get the header: Object.keys()  can change data[0] to [1]...
+	 * data[0] grab the first row. */
 		var headRow = Object.keys(data[0]);
-		var FirstRow = Object.keys(data[1]);
 		console.log(headRow);
-		console.log(FirstRow);
+	/*manipulate data row-by-row: data.map(): map function will iterate through 
+	 * each element of the data, then perform transformation on d and the transformation will
+	 * be applied to an entire data set. */
+		data.map(function(d) {
+			if(d['TIME_SPAN']== '1') {
+				console.log('this is the length of TIME_SPAN 1' +d['DNS']);
+			}
+		});
 	});
 });
-
 
