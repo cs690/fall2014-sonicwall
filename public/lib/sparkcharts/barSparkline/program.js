a8365a8e8a0cfe770a7a33e146326c7a9b891cf6
Michelle Gao <MichelleGao@Michelles-MacBook-Pro.local>
2014-11-02 19:45:24 -0800

    add thumbnails for sortable tables

--- public/lib/sparkcharts/barSparkline/program.js
+++ public/lib/sparkcharts/barSparkline/program.js
@@ -3,7 +3,9 @@ var values = [5, 4, 5, -2, 0, 3];
 // Draw a sparkline for the #sparkline element
  $('#sparkline').sparkline(values, {
      type: "bar",
-         tooltipSuffix: " widgets"
-         });
+	 height:'80px',
+	 barWidth:20
+     //tooltipSuffix: " widgets"
+       });
 });
 
